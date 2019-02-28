378230d7434d56d751b3997c8956ef91a811beb3
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-02-28 09:03:47 +0100

    Adds prettier

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # League of Legends Client
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/malcodeman/league-of-legends-client/blob/master/LICENSE)
+
 Web version of league of legends game client.
 
 Made with [hextech style guide](http://nexus.leagueoflegends.com/2016/12/the-visual-language-of-hextech/) in mind.
@@ -8,15 +12,19 @@ Made with [hextech style guide](http://nexus.leagueoflegends.com/2016/12/the-vis
 ## Assets
 
 ### Login Screens
-* [Blood Moon Diana](https://www.youtube.com/watch?v=myEKQkKdKtQ)
-* [Bit Rush: Arcade Ahri](https://www.youtube.com/watch?v=DYW-kEiCnJI)
+
+- [Blood Moon Diana](https://www.youtube.com/watch?v=myEKQkKdKtQ)
+- [Bit Rush: Arcade Ahri](https://www.youtube.com/watch?v=DYW-kEiCnJI)
 
 ### Fonts
-* [Roboto](https://fonts.google.com/specimen/Roboto)
+
+- [Roboto](https://fonts.google.com/specimen/Roboto)
 
 ### Logos
-* [Riot](https://www.riotgames.com/sites/default/files/asset-kit-assets/files/Riot_logo_WHITE_TEXT.png)
-* [League of Legends](https://www.riotgames.com/sites/default/files/asset-kit-assets/files/LOL_LOGO_RGB_SMALL%20copy.PNG)
+
+- [Riot](https://www.riotgames.com/sites/default/files/asset-kit-assets/files/Riot_logo_WHITE_TEXT.png)
+- [League of Legends](https://www.riotgames.com/sites/default/files/asset-kit-assets/files/LOL_LOGO_RGB_SMALL%20copy.PNG)
 
 ## License
+
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,116 @@
 // Animation
 
 function setAnimationState(animations_state) {
-    "use strict";
-    localStorage.setItem("animation_state", animations_state);
+  "use strict";
+  localStorage.setItem("animation_state", animations_state);
 }
 
 function getAnimationState() {
-    "use strict";
-    return localStorage.getItem("animation_state");
+  "use strict";
+  return localStorage.getItem("animation_state");
 }
 
 function loadAnimationState() {
-    "use strict";
-    var login_screen, animation_state, disable_animations;
-    login_screen = document.getElementById("login_screen");
-    animation_state = getAnimationState();
-    disable_animations = document.getElementById("disable_animations");
-    if (animation_state === "play") {
-        login_screen.play();
-        disable_animations.checked = false;
-    } else {
-        login_screen.currentTime = 1;
-        login_screen.pause();
-        disable_animations.checked = true;
-    }
+  "use strict";
+  var login_screen, animation_state, disable_animations;
+  login_screen = document.getElementById("login_screen");
+  animation_state = getAnimationState();
+  disable_animations = document.getElementById("disable_animations");
+  if (animation_state === "play") {
+    login_screen.play();
+    disable_animations.checked = false;
+  } else {
+    login_screen.currentTime = 1;
+    login_screen.pause();
+    disable_animations.checked = true;
+  }
 }
 
 function disableAnimations() {
-    "use strict";
-    var login_screen = document.getElementById("login_screen");
-    if (login_screen.paused) {
-        login_screen.play();
-        setAnimationState("play");
-    } else {
-        login_screen.pause();
-        setAnimationState("pause");
-    }
+  "use strict";
+  var login_screen = document.getElementById("login_screen");
+  if (login_screen.paused) {
+    login_screen.play();
+    setAnimationState("play");
+  } else {
+    login_screen.pause();
+    setAnimationState("pause");
+  }
 }
 
 // Music
 
 function setMusicState(music_state) {
-    "use strict";
-    localStorage.setItem("music_state", music_state);
+  "use strict";
+  localStorage.setItem("music_state", music_state);
 }
 
 function getMusicState() {
-    "use strict";
-    return localStorage.getItem("music_state");
+  "use strict";
+  return localStorage.getItem("music_state");
 }
 
 function loadMusicState() {
-    "use strict";
-    var login_music, music_state, disable_music;
-    login_music = document.getElementById("login_music");
-    music_state = getMusicState();
-    disable_music = document.getElementById("disable_music");
-    if (music_state === "play") {
-        login_music.play();
-        disable_music.checked = false;
-    } else {
-        login_music.pause();
-        disable_music.checked = true;
-    }
+  "use strict";
+  var login_music, music_state, disable_music;
+  login_music = document.getElementById("login_music");
+  music_state = getMusicState();
+  disable_music = document.getElementById("disable_music");
+  if (music_state === "play") {
+    login_music.play();
+    disable_music.checked = false;
+  } else {
+    login_music.pause();
+    disable_music.checked = true;
+  }
 }
 
 function disableMusic() {
-    "use strict";
-    var login_music = document.getElementById("login_music");
-    if (login_music.paused) {
-        login_music.play();
-        setMusicState("play");
-    } else {
-        login_music.pause();
-        setMusicState("pause");
-    }
+  "use strict";
+  var login_music = document.getElementById("login_music");
+  if (login_music.paused) {
+    login_music.play();
+    setMusicState("play");
+  } else {
+    login_music.pause();
+    setMusicState("pause");
+  }
 }
 
 // Sign in button
 
 function activeSignIn() {
-    "use strict";
-    var username, password, sign_in;
-    username = document.getElementById("username");
-    password = document.getElementById("password");
-    sign_in = document.getElementById("sign_in");
-    if (username.value.length > 0 && password.value.length > 0) {
-        sign_in.classList.add("active");
-    } else {
-        if (sign_in.classList.contains("active")) {
-            sign_in.classList.remove("active");
-        }
+  "use strict";
+  var username, password, sign_in;
+  username = document.getElementById("username");
+  password = document.getElementById("password");
+  sign_in = document.getElementById("sign_in");
+  if (username.value.length > 0 && password.value.length > 0) {
+    sign_in.classList.add("active");
+  } else {
+    if (sign_in.classList.contains("active")) {
+      sign_in.classList.remove("active");
     }
+  }
 }
 
 function main() {
-    "use strict";
-    document.getElementById("disable_animations").addEventListener("click", disableAnimations);
-    document.getElementById("disable_music").addEventListener("click", disableMusic);
-    document.getElementById("username").addEventListener("input", activeSignIn);
-    document.getElementById("password").addEventListener("input", activeSignIn);
-    if (getAnimationState() === null) {
-        setAnimationState("play");
-    }
-    if (getMusicState() === null) {
-        setMusicState("play");
-    }
-    loadAnimationState();
-    loadMusicState();
+  "use strict";
+  document
+    .getElementById("disable_animations")
+    .addEventListener("click", disableAnimations);
+  document
+    .getElementById("disable_music")
+    .addEventListener("click", disableMusic);
+  document.getElementById("username").addEventListener("input", activeSignIn);
+  document.getElementById("password").addEventListener("input", activeSignIn);
+  if (getAnimationState() === null) {
+    setAnimationState("play");
+  }
+  if (getMusicState() === null) {
+    setMusicState("play");
+  }
+  loadAnimationState();
+  loadMusicState();
 }
 main();
